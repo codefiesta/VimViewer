8563d5bd5f66aa1a3a2091fc1a0a0b44ff84979d
Kevin McKee <kevinmmckee@gmail.com>
2024-10-11 17:23:34 -0400

    Build lint fixes

--- VimViewer/Classes/Models/VimModelContainer.swift
+++ VimViewer/Classes/Models/VimModelContainer.swift
@@ -20,7 +20,10 @@ class VimModelContainer {
     private init() {
         let schema: Schema = .init(models)
         let configuration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: true)
-        modelContainer = try! ModelContainer(for: schema, configurations: [configuration])
+        guard let modelContainer = try? ModelContainer(for: schema, configurations: [configuration]) else {
+            fatalError("💩 Unabled to create ModelContainer")
+        }
+        self.modelContainer = modelContainer
 
         // Insert mock data
         let modelContext: ModelContext = ModelContext(modelContainer)
