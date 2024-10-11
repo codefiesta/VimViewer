dea47d27dd606b36eeb1062ecaf4bc41b8b573a6
Kevin McKee <17772633+codefiesta@users.noreply.github.com>
2024-10-11 11:24:08 -0400

    Initial Commit

---
+++ VimViewer/VimViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  VimViewerApp.swift
+//  VimViewer
+//
+//  Created by Kevin McKee on 10/11/24.
+//
+
+import SwiftUI
+
+@main
+struct VimViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ VimViewerTests/VimViewerTests.swift
@@ -0,0 +1,16 @@
+//
+//  VimViewerTests.swift
+//  VimViewerTests
+//
+//  Created by Kevin McKee on 10/11/24.
+//
+
+import Testing
+
+struct VimViewerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
