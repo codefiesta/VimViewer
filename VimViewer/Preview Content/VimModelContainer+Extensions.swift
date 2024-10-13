e10af501a2801162eea4ce0901bad05b0015c041
Kevin McKee <kevinmmckee@gmail.com>
2024-10-13 13:04:48 -0400

    Updating the sample file links

--- VimViewer/Preview Content/VimModelContainer+Extensions.swift
+++ VimViewer/Preview Content/VimModelContainer+Extensions.swift
@@ -16,13 +16,13 @@ extension VimModelContainer {
             VimModel(name: "Residence",
                          url: URL(string: "https://vim02.azureedge.net/samples/residence.v1.2.75.vim")!),
             VimModel(name: "Dwelling",
-                         url: URL(string: "https://vim02.azureedge.net/samples/dwelling.vim")!),
+                         url: URL(string: "https://www.dropbox.com/scl/fi/fgfdtc2r42wy8p3tpd5da/dwelling.vim?rlkey=osryntcuqvnj5qypyh4hb96xp&st=3m8xdu9t&dl=1")!),
             VimModel(name: "Medical Tower",
                          url: URL(string: "https://vim02.azureedge.net/samples/skanska.vim")!),
             VimModel(name: "Substation",
-                         url: URL(string: "https://vim02.azureedge.net/samples/substation.vim")!),
+                         url: URL(string: "https://www.dropbox.com/scl/fi/whi5faawkxjr9xiq148si/substation.vim?rlkey=xdfhuvq6xvd3sd9ffr3ouu1uj&st=gm7q1ite&dl=1")!),
             VimModel(name: "Snowdon Towers",
-                         url: URL(string: "https://vim02.azureedge.net/samples/snowden.vim")!),
+                         url: URL(string: "https://www.dropbox.com/scl/fi/cf4t5o0cjyxdn7ofpngsy/snowden.vim?rlkey=n368sb983zvpebz1pr9iic3ax&st=euigrt62&dl=1")!),
             VimModel(name: "Stadium",
                          url: URL(string: "https://vim02.azureedge.net/samples/stadium.vim")!)
         ]
